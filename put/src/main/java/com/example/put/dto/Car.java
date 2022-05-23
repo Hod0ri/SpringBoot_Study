6aef55aa4dd4bf30ccdf20739168c5dd8a907aa0
hod0ri <hodori991004@gmail.com>
2022-05-23 19:56:58 +0900

    [ SpringBoot ] PUT/DELETE and Response(raw Text, Json, ResponseEntity)

---
+++ put/src/main/java/com/example/put/dto/Car.java
@@ -0,0 +1,16 @@
+package com.example.put.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class Car {
+    private String name;
+    private String carNumber;
+}
