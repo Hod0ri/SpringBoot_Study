4f6532050d4b7494fa08855c4a81460001584541
Hod0ri <minthod0ri@gmail.com>
2022-10-14 21:38:02 +0900

    [ Spring boot ] Oauth2 Server -1

---
+++ Security/oauthServer/src/main/java/com/hod0ri/oauthserver/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.hod0ri.oauthserver.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+}
---
+++ Security/oauthServer/src/main/java/com/hod0ri/oauthserver/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.hod0ri.oauthserver.service;
+
+import com.hod0ri.oauthserver.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findB(username);
+        return null;
+    }
+}
